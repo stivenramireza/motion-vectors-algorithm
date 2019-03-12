f7036f98b76c29f4f501bcf3add0d81d8aadb513
Stiven Ramírez Arango <sramir70@eafit.edu.co>
2019-03-12 11:00:07 -0500

    Añadiendo primer acercamiento a la solución de MPI

--- mpi/src/bmp.cpp
+++ mpi/src/bmp.cpp
@@ -58,38 +58,36 @@ Image readBMP(const char* filename){
 void algorithm(Image im1, Image im2){
     ValueResult* matrixResults[im1.height/16][im1.width/16];
         int i,j,u,l,summation,k,y;
-        long nextWorker;
+        int nextW;
         for(i = 0; i < im1.height;i+=16){
-            nextWorker = nextWorker(); 
-            MPI_Send(&i, 1, MPI_INT, nextWorker, FROM_MASTER, MPI_COMM_WORLD); // dest = worker 1
+            nextW = nextWorker(); 
+            MPI_Send(&i, 1, MPI_INT, nextW, FROM_MASTER, MPI_COMM_WORLD); // dest = worker 1
             for(j = 0; j < im1.width; j+=16){
-                MPI_Send(&j, 1, MPI_INT, nextWorker, FROM_MASTER, MPI_COMM_WORLD); // dest = worker 1
+                MPI_Send(&j, 1, MPI_INT, nextW, FROM_MASTER, MPI_COMM_WORLD); // dest = worker 1
                 ValueResult* dataFrame = new ValueResult();
                 dataFrame->minimum = 2147483647; // Maximum value for a variable of type int.
                 for(u = 0; u < im2.height-16; u++){
-                    nextWorker = nextWorker();
-                    MPI_Send(&u, 1, MPI_INT, nextWorker, FROM_WORKER, MPI_COMM_WORLD); // dest = worker 2
+                    nextW = nextWorker();
+                    MPI_Send(&u, 1, MPI_INT, nextW, FROM_WORKER, MPI_COMM_WORLD); // dest = worker 2
                     for(l = 0; l < im2.width-16 ; l++){
-                        MPI_Send(&l, 1, MPI_INT, nextWorker, FROM_WORKER, MPI_COMM_WORLD); // dest = worker 2
+                        MPI_Send(&l, 1, MPI_INT, nextW, FROM_WORKER, MPI_COMM_WORLD); // dest = worker 2
                         summation = 0;
-                        MPI_Send(&summation, 1, MPI_INT, nextWorker, FROM_WORKER, MPI_COMM_WORLD);
+                        MPI_Recv(&u, 1, MPI_INT, nextW, FROM_WORKER, MPI_COMM_WORLD, &status); // source = worker 1
+                        MPI_Recv(&l, 1, MPI_INT, nextW, FROM_WORKER, MPI_COMM_WORLD, &status); // source = worker 1
+                        MPI_Recv(&i, 1, MPI_INT, MASTER, FROM_MASTER, MPI_COMM_WORLD, &status); // source = master
+                        MPI_Recv(&j, 1, MPI_INT, MASTER, FROM_MASTER, MPI_COMM_WORLD, &status); // source = master
                         for(k = 0; k < 16; k++){
                             for(y = 0; y < 16; y++){
-                                MPI_Recv(&summation, 1, MPI_INT, nextWorker, FROM_WORKER, MPI_COMM_WORLD, &status); // source = worker 1
-                                MPI_Recv(&u, 1, MPI_INT, nextWorker, FROM_WORKER, MPI_COMM_WORLD, &status); // source = worker 1
-                                MPI_Recv(&l, 1, MPI_INT, nextWorker, FROM_WORKER, MPI_COMM_WORLD, &status); // source = worker 1
-                                MPI_Recv(&i, 1, MPI_INT, MASTER, FROM_MASTER, MPI_COMM_WORLD, &status); // source = master
-                                MPI_Recv(&j, 1, MPI_INT, MASTER, FROM_MASTER, MPI_COMM_WORLD, &status); // source = master
                                 summation += abs(im1.arrayOfPixels[getIndex(i+k,j+y,im1.width)] - im2.arrayOfPixels[getIndex(u+k,l+y,im2.width)]);
-                                MPI_Send(&summation, 1, MPI_INT, MASTER, FROM_WORKER, MPI_COMM_WORLD); // dest = master
+                                MPI_Send(&summation, 1, MPI_INT, nextW, FROM_WORKER, MPI_COMM_WORLD); // dest = worker 2
                             }
                         }
-                        MPI_Recv(&summation, 1, MPI_INT, MASTER, FROM_WORKER, MPI_COMM_WORLD, &status); // source = worker 2
+                        MPI_Recv(&summation, 1, MPI_INT, MASTER, FROM_MASTER, MPI_COMM_WORLD, &status); // source = worker 2
                         if(summation < dataFrame->minimum){
                             dataFrame->minimum = summation;
                             dataFrame->iFrame2 = u;
-                            dataFrame->jFrame2 = l;           
-
+                            dataFrame->jFrame2 = l;          
+                             
                             if(summation == 0) goto endFrame2;
                         }
                     }
