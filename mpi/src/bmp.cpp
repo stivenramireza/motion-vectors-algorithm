b49ee2eb08bc9d07f3529d3f9895ff773f3e11c4
Stiven Ramírez Arango <sramir70@eafit.edu.co>
2019-03-17 20:38:30 -0500

    Probando mpi en el cluster

--- mpi/src/bmp.cpp
+++ mpi/src/bmp.cpp
@@ -26,7 +26,7 @@ Image readBMP(const char* filename){
 
     unsigned short int  bits = *(short *) &info[28];
     if(bits != 8){
-        printf("Error, The image has to be of 8 Bits");
+        //printf("Error, The image has to be of 8 Bits");
         exit (EXIT_FAILURE);
     }
     
@@ -77,11 +77,11 @@ void algorithm(int height, unsigned char frame1[],int sizeFrame1,int frame1H,int
             }
             endFrame2:  
             matrixResults[i/16][j/16] = dataFrame;
-            if(getout >= sizeFrame1) goto endExecution;
+            //if(getout >= sizeFrame1) goto endExecution;
         }
     }
-    endExecution:
-    printf("end execution from process %i \n",taskId);
+    //endExecution:
+    //printf("end execution from process %i \n",taskId);
 }
 
 int main(int argc, char *argv[]){
@@ -93,7 +93,7 @@ int main(int argc, char *argv[]){
     ValueResult* matrix_procesada[im1.height/16][im1.width/16];
 
     if((im1.width != im2.width) && (im1.height != im2.height)){
-        printf("Error, The images have to be with the same width and height, Try with other images");
+        //printf("Error, The images have to be with the same width and height, Try with other images");
         exit (EXIT_FAILURE);
     }
 
@@ -113,18 +113,18 @@ int main(int argc, char *argv[]){
         extraMacroBlock = totalMacroBlock - macroPerN*numtasks ; //extra iterations
         macroblockSize = 16*16;
         int iterationsPerN = macroPerN*macroblockSize;
-        printf(" primera posicion: %i \n",im1.arrayOfPixels[0]);
+        //printf(" primera posicion: %i \n",im1.arrayOfPixels[0]);
         for(int dest = 1; dest < numtasks; dest++){
             MPI_Send(&macroPerN, 1, MPI_INT, dest, 1, MPI_COMM_WORLD);
             MPI_Send(&im1.height, 1, MPI_INT, dest, 1, MPI_COMM_WORLD);
             MPI_Send(&im1.width, 1, MPI_INT, dest, 1, MPI_COMM_WORLD);
             MPI_Send(&im2.height, 1, MPI_INT, dest, 1, MPI_COMM_WORLD);
             MPI_Send(&im2.width, 1, MPI_INT, dest, 1, MPI_COMM_WORLD);
-            printf("dest %i \n",dest);
+            //printf("dest %i \n",dest);
             MPI_Send(&im1.arrayOfPixels[macroPerN*macroblockSize*dest], macroPerN*macroblockSize, MPI_CHAR, dest, 1, MPI_COMM_WORLD);
             MPI_Send(im2.arrayOfPixels, N, MPI_CHAR, dest, 1, MPI_COMM_WORLD);
         }
-        printf("task 0: %i \n",im1.arrayOfPixels[0]);
+        //printf("task 0: %i \n",im1.arrayOfPixels[0]);
         algorithm(height_total, im1.arrayOfPixels,macroPerN,im1.height,im1.width,im2.arrayOfPixels,im2.height,im2.width,0);
         if(extraMacroBlock > 0){
             //do something
@@ -148,9 +148,9 @@ int main(int argc, char *argv[]){
         unsigned char im1Array[macroPerN*macroblockSize]; 
         MPI_Recv(im1Array, macroPerN*macroblockSize, MPI_CHAR, source, 1, MPI_COMM_WORLD, &status);
         MPI_Recv(im2Array, N, MPI_CHAR, source, 1, MPI_COMM_WORLD, &status);
-        printf("primer valor: %i por procesador: %i\n",im1Array[0],taskid);
-        printf("tamaño de array de macrobloques: %i tamaño de frame2: %i\n",(sizeof(im1Array)/sizeof(*im1Array)),(sizeof(im2Array)/sizeof(*im2Array)));
-        printf("task 0: %i \n",im1Array[0]);
+        //printf("primer valor: %i por procesador: %i\n",im1Array[0],taskid);
+        //printf("tamaño de array de macrobloques: %i tamaño de frame2: %i\n",(sizeof(im1Array)/sizeof(*im1Array)),(sizeof(im2Array)/sizeof(*im2Array)));
+        //printf("task 0: %i \n",im1Array[0]);
 
         algorithm(height_total, im1Array,macroPerN,heightIm1,widthIm1,im2Array,heightIm2,widthIm2,taskid);
     }   
